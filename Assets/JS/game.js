054d8781a88121dddbe54df6f3d6acc3ceaf53c6
Gold Dust <48570104+ashley-blue@users.noreply.github.com>
2019-06-01 13:56:38 -0600

    added basic html and images added to Images folder

---
+++ Assets/JS/game.js
@@ -0,0 +1,17 @@
+var Wins = 0;
+var Losses = 0;
+var Total = 0;
+var RandomStartNum = [];
+var HiddenValueNum = [];
+//you will need a var for crystal worth (maybe one var for all or one var per goal number)
+//generate a random number at start of game
+function startGame() {
+
+}
+//set a hidden number value to each crystal
+function getNumber() {
+
+}
+//on click of crystal add hidden number value to total score area
+//when a new crystal is clicked and there is a value present in score section, values will need to be added together and displayed
+//once player gets either correct number or does not, a win or loss message should be displayed

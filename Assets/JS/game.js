2ece0d30aa19808d1935a42f68984c11eb37e605
Gold Dust <48570104+ashley-blue@users.noreply.github.com>
2019-06-04 21:40:39 -0600

    buttons added but do not function yet

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -15,3 +15,7 @@ function getNumber() {
 //on click of crystal add hidden number value to total score area
 //when a new crystal is clicked and there is a value present in score section, values will need to be added together and displayed
 //once player gets either correct number or does not, a win or loss message should be displayed
+
+$(document).ready(function(){
+    $(":button").css();
+  });
